4f81d8acddc28fab13314d172d3f09b80555ce10
Qiuhan Wang <qiuhanwang2023@163.com>
2023-08-10 19:24:29 +0800

    feat(master): 为LayerCard增加新模式，可以选择显示按钮与否

--- src/pages/TmsLayer/components/LayerCard/index.js
+++ src/pages/TmsLayer/components/LayerCard/index.js
@@ -3,36 +3,56 @@ import { Button } from 'antd';
 import styles from './index.less';
 
 const LayerCard = (props) => {
-  const onClick = () => {
-    props.activeLayerOnChange(props.layer);
-  };
-  return (
-    <ProCard
-      type="inner"
-      title={props.layer.name}
-      extra={<Button onClick={onClick}>切换</Button>}
-      bordered={true}
-      className={styles.layerCard}
-      direction="column"
-    >
-      <h4>DESCRIPTION</h4>
-      <p>{props.layer.description}</p>
-      <h4>URL</h4>
-      <div className={styles.codeBox}>
-        <code>{props.layer.url}</code>
-      </div>
-    </ProCard>
-  );
+  if (props.btn) {
+    const onClick = () => {
+      props.activeLayerOnChange(props.layer);
+    };
+    return (
+      <ProCard
+        type="inner"
+        title={props.layer.name}
+        extra={<Button onClick={onClick}>切换</Button>}
+        bordered={true}
+        className={styles.layerCard}
+        direction="column"
+      >
+        <h4>DESCRIPTION</h4>
+        <p>{props.layer.description}</p>
+        <h4>URL</h4>
+        <div className={styles.codeBox}>
+          <code>{props.layer.url}</code>
+        </div>
+      </ProCard>
+    );
+  } else {
+    return (
+      <ProCard
+        type="inner"
+        title={props.layer.name}
+        extra="provided by DDE"
+        bordered={true}
+        className={styles.layerCard}
+        direction="column"
+      >
+        <h4>DESCRIPTION</h4>
+        <p>{props.layer.description}</p>
+        <h4>URL</h4>
+        <div className={styles.codeBox}>
+          <code>{props.layer.url}</code>
+        </div>
+      </ProCard>
+    );
+  }
 };
 
 export default LayerCard;
-{
-  /* <Space direction="vertical" size="small" style={{ display: 'flex' }}>
-        <ProCard title="DESCRIPTION">{props.layer.description}</ProCard>
-        <ProCard title="URL">
-          <div className={styles.codeBox}>
-            <code>{props.layer.url}</code>
-          </div>
-        </ProCard>
-      </Space> */
-}
+
+LayerCard.defaultProps = {
+  layers: {
+    uid: 'default',
+    name: 'default',
+    url: 'default',
+    description: 'default',
+  },
+  btn: false,
+};
